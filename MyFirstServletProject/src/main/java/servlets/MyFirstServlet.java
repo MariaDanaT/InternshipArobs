60d6a3bfdac2eeec95a684ce7b81658ec3cdb91e
MariaDanaT <m.tomos@yahoo.com>
2022-03-21 16:52:07 +0200

    Add Logback for doGet and doPost methods

--- MyFirstServletProject/src/main/java/servlets/MyFirstServlet.java
+++ MyFirstServletProject/src/main/java/servlets/MyFirstServlet.java
@@ -1,5 +1,9 @@
 package servlets;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -12,11 +16,14 @@ import java.io.IOException;
 @WebServlet(name = "MyFirstServlet", urlPatterns = "/myPage")
 public class MyFirstServlet extends HttpServlet {
 
+    private static final Logger logger
+            = LoggerFactory.getLogger(MyFirstServlet.class);
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletContext servletContext =getServletContext();
         RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/jsp/myPage.jsp");
         requestDispatcher.forward(req,resp);
+        logger.info("Call method doGet!");
     }
 
     @Override
@@ -30,5 +37,7 @@ public class MyFirstServlet extends HttpServlet {
         ServletContext servletContext =getServletContext();
         RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/jsp/myPage.jsp");
         requestDispatcher.forward(req,resp);
+
+        logger.info("Call method doPost!");
     }
 }
