b85265af98d163da0a7d67b43c2405194170a61d
MariaDanaT <m.tomos@yahoo.com>
2022-03-24 15:23:57 +0200

    Create basic connection pool using Hikari library

--- DataBases/Library/src/main/java/jdbc/Main.java
+++ DataBases/Library/src/main/java/jdbc/Main.java
@@ -5,13 +5,23 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
 
+//        ConnectionPool basicConnection = new BasicConnection("jdbc:mysql://localhost:3306/Library", "root", "password",3);
+        ConnectionPool basicConnection = new HikariConnection("jdbc:mysql://localhost:3306/Library", "root", "password",3);
+
+        Queries queries = new Queries();
+//        System.out.println("Insert book: "+queries.insertBook("Maitreyi", "roman"));
+        System.out.println("Update mail: " + queries.updateMail(1, "aana@gmail.com"));
+
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Library", "root", "password");
+//            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Library", "root", "password");
+            connection = basicConnection.getConnection();
             if (connection != null) {
                 System.out.println("Connected successfully!");
                 Statement statement = connection.createStatement();
-                String query = "SELECT * FROM people";
+                String id1 = "1' or 1=1"; //in this way, all rows from people table are printed
+                String query = "SELECT * FROM people WHERE id = '" + id1;
+
                 ResultSet rs = statement.executeQuery(query);
 
                 while (rs.next()) {
@@ -30,11 +40,16 @@ public class Main {
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
+//            try {
+//                connection.close();
             try {
-                connection.close();
+                basicConnection.releaseConnection(connection);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
         }
     }
 }
